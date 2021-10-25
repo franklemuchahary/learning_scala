7f9ae0e1e9e0801e5544eaa117720b9f56e202b1
Frankle Muchahary <franklemuchahary@gmail.com>
2021-10-26 01:15:53 +0530

    adding more recursion exaples

---
+++ functional_programming_in_scala/filter_implementation_tail_recursion.scala
@@ -0,0 +1,41 @@
+import java.io._
+import scala.io._
+
+object Solution {
+	def filter_implement(delim:Int, arr:List[Int]):List[Int] = {
+		/// manual implementation of basic filter function in a functional style for comparing less than
+	    arr match {
+	        case arr if arr.length == 0 => arr
+	        case arr if arr.head < delim => List(arr.head) ++ filter_implement(delim, arr.tail)
+	        case arr => filter_implement(delim, arr.tail)
+	    }
+	}
+
+	
+	def filter_tail(delim:Int, arr:List[Int], res:List[Int]):List[Int] = {
+	    /// tail recursion implementation of the above function -> scala automatically optimizes this to use only one stack frame
+	    arr match {
+	        case arr if arr.length == 0 => 
+	            res
+	        case arr if arr.head < delim => 
+	            filter_tail(delim, arr.tail, res ++ List[Int](arr.head))
+	        case arr => 
+	            filter_tail(delim, arr.tail, res)
+	    }
+	}
+
+	def test(delim:Int, arr:List[Int]){
+		println(delim)
+		println(arr)
+		println(delim.getClass)
+		println(arr.getClass)
+	}
+
+	def main(args : Array[String]){
+		var num = StdIn.readLine.trim.toInt
+		var arr = StdIn.readLine.split(" ").map(x => x.trim.toInt).toList
+		var result = filter_tail(num, arr, List[Int]())	
+		println(result)
+	}
+}
+
